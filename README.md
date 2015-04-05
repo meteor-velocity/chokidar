9e01843c381ca423924d17c4f1413247579704db
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-05 23:13:47 +0200

    Release of 0.12.6_1

---
+++ README.md
@@ -0,0 +1,8 @@
+# velocity:chokidar
+
+The npm module chokidar packaged as Meteor package.
+
+## Why?
+
+Chokidar depends on fsevents. When you publish your package you need to
+build architecture dependent versions. Using this package removes this step.
---
+++ package.js
@@ -0,0 +1,22 @@
+var chokidarVersion = '0.12.6';
+
+Package.describe({
+  name: 'velocity:chokidar',
+  version: chokidarVersion + '_1',
+  // Brief, one-line summary of the package.
+  summary: 'The npm module chokidar packaged as Meteor package.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/meteor-velocity/chokidar.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Npm.depends({
+  'chokidar': chokidarVersion
+});
+
+Package.onUse(function(api) {
+  api.export('chokidar', 'server');
+  api.addFiles('chokidar.js');
+});
